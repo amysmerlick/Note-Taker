e218d76c69d36a12ba2ee838a541814b5541122b
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-05-15 14:06:16 -0400

    update files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiroutes.js')
-const htmlRoutes = require('./routes/htmlroutes')
+const htmlRoutes = require('./routes/htmlroutes.js')
 
 
 const app = express();
